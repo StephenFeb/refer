f9555c1ff2a8a8c4a300c936c2988eb95682af86
StephenFeb <spinshon@hotmail.com>
2020-06-08 22:12:07 +0100

    user and job models

---
+++ db/migrate/20200608193346_create_jobs.rb
@@ -0,0 +1,15 @@
+class CreateJobs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.string :organisation
+      t.string :location
+      t.integer :salary
+      t.text :description
+      t.text :skills
+      t.text :culture
+
+      t.timestamps
+    end
+  end
+end
