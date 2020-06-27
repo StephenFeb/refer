2645d9921badc6f5a9e77f56caa536e67b974042
StephenFeb <spinshon@hotmail.com>
2020-06-27 23:44:23 +0100

    Jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,19 @@
 class JobsController < ApplicationController
 
     def index
+        @Jobs = Job.all
     end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def destroy
+    end
+
 end
